8aa584653518431e3e27bfd42b83f355b2ef4d30
vishal tomar <116427576+VishalHarry@users.noreply.github.com>
2024-09-29 02:33:16 +0530

    add new pages

--- FinalPorjectOfJS/script.js
+++ FinalPorjectOfJS/script.js
@@ -53,11 +53,11 @@ tl.from("#nav",{
    
 })
 tl.from(".hero1 h1,.hero2 h1,.hero3 h2,.hero4 h1",{
-    y:120,
+    y:140,
     stagger:0.2
 })
 }
-loading();
+ loading();
 
 
 function curcureAnimation(){
